3c7377b55e8585263ed4f328dbb92a139c6aa1f0
Kakaique2000 <kaique.lisboa@gmail.com>
2020-06-15 21:27:08 -0300

    comentado e parametros

--- EP - IA/src/ia/Weka.java
+++ EP - IA/src/ia/Weka.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import weka.core.Utils;
 import weka.core.converters.CSVLoader;
@@ -14,16 +16,18 @@ import weka.core.Instances;
 public class Weka {
 
 
+	// Função responsável por carregar um arquivo CSV através de um caminho (filePath)
 	public static CSVLoader loader(String filepath) throws IOException {
 		
-		CSVLoader trainloader = new CSVLoader();
+		CSVLoader loader = new CSVLoader();
+
+		loader.setSource(new File(filepath));
+		loader.setNoHeaderRowPresent(true);
 		
-		trainloader.setSource(new File(filepath));
-		trainloader.setNoHeaderRowPresent(true);
-		
-		return trainloader;
+		return loader;
 	}
-	
+
+	// Função responsável por transformar os carregamentos CSV em instâncias para serem lidas pelo classificador
 	public static Instances instancia (CSVLoader loader) throws IOException {
 		
 		Instances train = loader.getDataSet();
@@ -31,12 +35,11 @@ public class Weka {
 		
 		return train;
 	}
-	
+
+	// Inicializa o Classificador do tipo MultiLayerPerceptron
 	public static MultilayerPerceptron inicializaMLP (double taxaAprendizado, double momentum, String camadasEscondidas) throws Exception {
 		
 		MultilayerPerceptron mlp = new MultilayerPerceptron();
-		
-		//Setting Parameters
 
 //		mlp.setGUI(true);
 		mlp.setLearningRate(taxaAprendizado);
@@ -46,12 +49,14 @@ public class Weka {
 
 		return mlp;
 	}
-	
+
+	// Função responsável por avaliar um conjunto de dados para o modelo treinado
 	public static Evaluation avalia (MultilayerPerceptron mlp, Instances data, OutputStrings os) throws Exception {
 
 		Evaluation eval = new Evaluation(data);
 		eval.evaluateModel(mlp, data);
 
+		//Passa por cada Dado para avaliar o resultado gerado
 		data.stream().forEach(d -> {
 			try {
 				Evaluation tmpEval = new Evaluation(data);
@@ -66,23 +71,26 @@ public class Weka {
 
 		os.outputDataResults += "\n\n" + eval.toSummaryString();
 
-	    //System.out.println("Taxa de erro: " + eval.errorRate()); //Printing Training Mean root squared Error
-	    //System.out.println(eval.toSummaryString("\nResults\n======\n", false)); //Summary of Training
-		//System.out.println(eval.getRevision());
+	    System.out.println(eval.toSummaryString("\nResults\n======\n", false)); //Summary of Training
 
 
 	    return eval;
 	}
 
+	//Função responsável por treinar a rede neural
 	public static MultilayerPerceptron treinaRede(MultilayerPerceptron mlp, Instances buildClassifier, int epocas, OutputStrings os) throws Exception {
 		mlp.initializeClassifier(buildClassifier);
 		os.outputInitWeights += mlp.toString();
+		System.out.println("Inicializando o treino...\n\n");
+
+		// Itera sobre cada época
 		for(int i=0; i<epocas; i++) {
 			System.out.println("\n==============================================\n============ Modelo na época " + i + " ==============\n==============================================\n");
 			System.out.println(mlp.toString());
 			Evaluation eval = new Evaluation(buildClassifier);
 			eval.evaluateModel(mlp, buildClassifier);
 			os.outputEpochErrors += ("\ntaxa de erro na época " + (i+1) + ": " + eval.errorRate()*100 + "%");
+			System.out.println("\ntaxa de erro na época " + (i+1) + ": " + eval.errorRate()*100 + "%");
 			mlp.next();
 		}
 		os.outputFinalWeights += mlp.toString();
@@ -93,57 +101,98 @@ public class Weka {
 	
 	public static void main(String[] args) {
 
-		double taxaAprendizado = 0.8;
-		double momentum = 0.2;
-		int epocas = 100;
-		String camadasEscondidas = "4";
-	
 		try {
+
+		    // Pega taxa de aprendizado do argumento -A ou mantem valor padrão (0.8)
+		    double taxaAprendizado = 0.8;
+            String taxaAprendizadoInput = (Utils.getOption('A', args));
+            if(taxaAprendizadoInput.length() != 0) {
+                taxaAprendizado = Double.parseDouble(taxaAprendizadoInput);
+            }
+
+            // Pega momentum do argumento -M ou mantem valor padrão (0.2)
+            double momentum = 0.2;
+            String momentumInput = (Utils.getOption('M', args));
+            if(momentumInput.length() != 0) {
+                momentum = Double.parseDouble(momentumInput);
+            }
+
+            // Pega o número de épocas do argumento -E ou mantem valor padrão (100)
+            int epocas = 100;
+            String epocasInput = (Utils.getOption('E', args));
+            if(epocasInput.length() != 0) {
+                epocas = Integer.parseInt(epocasInput);
+            }
+
+            // Pega padrão de camadas escondidas do argumento -C ou mantem valor padrão (6)
+            String camadasEscondidas = (Utils.getOption('C', args));
+            if(camadasEscondidas.length() == 0) {
+                camadasEscondidas = "6";
+            } else {
+                if(!Pattern.compile("^([0-9]+,)*[0-9]+$").matcher(camadasEscondidas).matches()) {
+                    throw new NumberFormatException("formato inválido para camadas, tente seguir o padrão ^([0-9]+,)*[0-9]+$");
+                }
+            }
+
+            // Pega caminho do arquivo a ser utilizado no treino
 			String arquivoTreino = Utils.getOption('t', args);
 			if(arquivoTreino.length() == 0) {
 				System.out.println("Por favor, informe um caminho para o arquivo de treino com \"-t <caminho_para_arquivo.csv>\"");
 				return;
 			}
+
+			//Pega caminho do arquivo a ser utilizado na execução
 			String arquivoExecucao = Utils.getOption('r', args);
 			if(arquivoTreino.length() == 0) {
 				System.out.println("Por favor, informe um caminho para o arquivo de execução com \"-r <caminho_para_arquivo.cvs>\"");
 				return;
 			}
+
+			// Pega diretório de despejo dos logs
 			String outputFolderLocation = Utils.getOption('o', args);
 			if(outputFolderLocation.length() == 0) {
 				System.out.println("Parâmetro de caminho de saída não informado: (-o), utilizando o caminho padrão (pasta atual)");
 				outputFolderLocation = ".";
 			}
 
+			// Instancia classe anônima para gravar os Logs
 			OutputStrings os = new OutputStrings();
 
+			// Define atributos iniciais da classe anônima
 			os.outputInitParams += "Taxa de aprendizado: " + taxaAprendizado;
 			os.outputInitParams += "\nmomentum: " + momentum;
 			os.outputInitParams += "\nCamadas escondidas, formato(numero de nos da primeira camada, numero de nos da segunda camada...): " + camadasEscondidas;
 			os.outputInitParams += "\nÉpocas: " + epocas;
 
-			//Reading training arff or csv file
+			// Lê arquivos de teste com função responsável pelo carregamento
 			CSVLoader trainLoader = loader(arquivoTreino);
 			CSVLoader runLoader = loader(arquivoExecucao);
 
+			// Cria as instâncias de treino e de modelo, necessárias para o treinamento do algoritmo
 			Instances train = instancia(trainLoader);
 			Instances model = instancia(runLoader);
 
 	
-			//Instance of NN
+			// Instancia o classificador passando os parâmetros iniciais
 			MultilayerPerceptron mlp = inicializaMLP(taxaAprendizado, momentum, camadasEscondidas);
 			mlp = treinaRede(mlp, train, epocas, os);
-			
+
+			// Avalia o modelo passado para execução
 			avalia(mlp, model, os);
 
+			// Dá output nos logs de execução
 			os.escreveArquivosDasStrings(outputFolderLocation);
 			System.out.println("Treino e execução finalizados, cheque o diretorio \"" + Paths.get(outputFolderLocation).toAbsolutePath() + "\" para ver os resultados");
 
 		}
-		
+		catch(NumberFormatException ne) {
+			System.out.println("Formato de número inválido!");
+			ne.printStackTrace();
+		}
 		catch(Exception ex) {
 			ex.printStackTrace();
 		}
+
 	}
 
 }
--- executavel/README.md
+++ executavel/README.md
@@ -3,3 +3,13 @@
 É necessário executar com o JRE na versão 11, para isso, basta apontar a variável de ambiente %JAVA_HOME% para a pasta de instalação do jdk 11 Ex: `C:\Program Files\Java\jdk-11\` e executar o comando a seguir:
 
 `"%JAVA_HOME%\bin\java" -jar MultilayerPerceptron.jar -t caracteres-limpo.csv -r caracteres-ruido.csv`
+
+### Argumentos:
+
+* `-t <caminho do arquivo de treino>` - Especifica caminho do arquivo de treino ***(Obrigatório)***
+* `-r <caminho do arquivo de execução>` - Especifica caminho do arquivo de execução ***(Obrigatório)***
+* `-o <caminho do diretório de output>` - Especifica o diretório em que o programa fará output dos logs, ***padrão: diretório atual***
+* `-A <taxa de aprendizado>` - Especifica a taxa de aprendizado do modelo, ***padrão: 0.8***
+* `-M <momentum>` - Especifica o momentum a ser utilizado no modelo, ***padrão: 0.2***
+* `-E <número de épocas>` - Especifica o número de épocas a serem utilizadas no treinamento, ***padrão: 100***
+* `-C <camadas>` - Especifica o modelo das camadas escondidas do modelo, *(ex: "6,5,8", "8", "6,5")*, ***padrão: 6***
